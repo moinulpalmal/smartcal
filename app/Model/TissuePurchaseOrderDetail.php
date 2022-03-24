271a76131e3ab3c15c7ae4eb5cbda8089dbce615
moinularif <arif.cuet.08@gmail.com>
2022-03-24 15:54:11 +0600

    Tissue PO Report Total_Value Add

--- app/Model/TissuePurchaseOrderDetail.php
+++ app/Model/TissuePurchaseOrderDetail.php
@@ -25,4 +25,22 @@ class TissuePurchaseOrderDetail extends Model
             ->groupBy('tissue_purchase_order_details.tissue_product_setup_id', 'tissue_product_setups.id', 'tissue_product_setups.name')
             ->get();
     }
+
+    public static function getSumTotalPrice($masterId){
+        $poDetails = DB::table('tissue_purchase_order_details')
+            ->select('tissue_purchase_order_details.order_quantity', 'tissue_purchase_order_details.total_price')
+            ->where('purchase_order_master_id', $masterId)
+            ->where('status' , '!=', 'D')
+            ->get();
+        //$total_order_quantity = 0;
+        $sum_total_price = 0;
+
+        foreach ($poDetails as $detail){
+            //$total_order_quantity = $total_order_quantity + (float)$detail->order_quantity;
+            $sum_total_price = $sum_total_price + (float)$detail->total_price;
+        }
+
+        return $sum_total_price;
+
+    }
 }
